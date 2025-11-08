057be84d67182499d771d6e18436ac785332c8c7
Rafael Messias <rafael@NoteRafael.localdomain>
2025-11-08 15:47:30 -0300

    Lab - Terraform + Ansible com botão de Destroy

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-	resource_group_name  = "rg-tfstate"
-	storage_account_name = "tfstatecurso24090"
-	container_name       = "tfstate"
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstatecurso24090"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
 }
